e1a482b7b4c87453cc56fe55dcff14f2a1a5b0a5
Michael Johnson <michael@nightexcessive.us>
2015-02-11 15:54:15 -0500

    Updated time-ago system and new fixtures to help test it

--- app/components/time-ago.js
+++ app/components/time-ago.js
@@ -1,6 +1,32 @@
 import Ember from "ember";
 import moment from "moment";
 
+var millisecond = 1;
+var second = millisecond*1000;
+var minute = second*60;
+var hour = minute*60;
+var day = hour*24;
+
+var diffBeforeFromNow = 45*second; // 30 seconds
+var diffBeforeTime = 6*hour; // 6 hours
+var diffBeforeDatetime = 20*hour; // 20 hours
+
+function nextFromNowChange(diff) {
+	if(diff < 45*second) {
+		return 45 * second - diff;
+	} else if(diff < 90*second) {
+		return 90 * second - diff;
+	} else if(diff < 45*minute) {
+		return minute - diff % minute;
+	} else if(diff < 90*minute) {
+		return 90 * minute - diff;
+	} else if(diff < 22*hour) {
+		return hour - diff % hour;
+	} else {
+		throw "can not yet calculate next change for "+diff+"ms";
+	}
+}
+
 export default Ember.Component.extend({
 	timeAgo: "",
 	oldTimeAgo: "",
@@ -8,18 +34,32 @@ export default Ember.Component.extend({
 	timer: null,
 	
 	startClock: function() {
+		this.cancelClock();
 		this.set("calculatedTime", moment(this.get("createdAt")));
 		this.clock();
-	}.on("didInsertElement"),
+	}.on("didInsertElement").observes("createdAt"),
 
 	clock: function() {
-		var newTimeAgo = this.get("calculatedTime").fromNow();
+		var diff = -(this.get("calculatedTime").diff());
+		var nextChange; // how long until this should change
 		
-		if (this.get("oldTimeAgo") !== newTimeAgo) {
-			this.set("timeAgo", newTimeAgo);
+		if(diff < diffBeforeFromNow) {
+			this.set("timeAgo", "Now");
+			nextChange = diffBeforeFromNow-diff;
+		} else if(diff < diffBeforeTime) {
+			this.set("timeAgo", this.get("calculatedTime").fromNow(true));
+			nextChange = nextFromNowChange(diff);
+		} else if(diff < diffBeforeDatetime) {
+			this.set("timeAgo", this.get("calculatedTime").format("LT"));
+			nextChange = diffBeforeDatetime-diff;
+		} else {
+			this.set("timeAgo", this.get("calculatedTime").format("l LT"));
+			nextChange = -1;
 		}
-		
-		this.set("timer", Ember.run.later(this, this.clock, 3000));
+
+		if(nextChange >= 0) {
+			this.set("timer", Ember.run.later(this, this.clock, nextChange+500));
+		}
 	},
 
 	cancelClock: function() {
--- app/models/message.js
+++ app/models/message.js
@@ -15,15 +15,29 @@ var Message = DS.Model.extend({
 	}.property("time")
 });
 
+var millisecond = 1;
+var second = millisecond*1000;
+var minute = second*60;
+var hour = minute*60;
+var day = hour*24;
+
+var diffBeforeFromNow = 45*second; // 30 seconds
+var diffBeforeTime = 6*hour; // 6 hours
+var diffBeforeDatetime = 20*hour; // 20 hours
+
+var actionTime = 5*second;
+
 Message.reopenClass({
 	FIXTURES: [
-		{id: "t2h1", conversation: "test2@nightexcessive.us", from: "test2@nightexcessive.us", to: "receiver@nightexcessive.us", time: new Date("2015-02-11T00:10:23.783Z"), message: "Hello 1"},
-		{id: "t2h2", conversation: "test2@nightexcessive.us", from: "test2@nightexcessive.us", to: "receiver@nightexcessive.us", time: new Date("2015-02-11T00:10:23.783Z"), message: "Hello 2"},
-		{id: "t2h3", conversation: "test2@nightexcessive.us", from: "test2@nightexcessive.us", to: "receiver@nightexcessive.us", time: new Date("2015-02-11T00:10:23.783Z"), message: "Hello 3"},
+		{id: "t2h1", conversation: "test2@nightexcessive.us", from: "test2@nightexcessive.us", to: "receiver@nightexcessive.us", time: moment().subtract(diffBeforeDatetime-actionTime).toDate(), message: "Hello 1"},
+		{id: "t2h2", conversation: "test2@nightexcessive.us", from: "test2@nightexcessive.us", to: "receiver@nightexcessive.us", time: moment().subtract(diffBeforeTime-actionTime).toDate(), message: "Hello 2"},
+		{id: "t2h3", conversation: "test2@nightexcessive.us", from: "test2@nightexcessive.us", to: "receiver@nightexcessive.us", time: moment().subtract(diffBeforeFromNow-actionTime).toDate(), message: "Hello 3"},
+		{id: "t2h4", conversation: "test2@nightexcessive.us", from: "test2@nightexcessive.us", to: "receiver@nightexcessive.us", time: moment().toDate(), message: "Hello 4"},
 
-		{id: "t1h1", conversation: "test1@nightexcessive.us", from: "test1@nightexcessive.us", to: "receiver@nightexcessive.us", time: new Date("2015-02-11T00:12:14.783Z"), message: "Hello 1"},
-		{id: "t1h2", conversation: "test1@nightexcessive.us", from: "test1@nightexcessive.us", to: "receiver@nightexcessive.us", time: new Date("2015-02-11T00:12:19.783Z"), message: "Hello 2"},
-		{id: "t1h3", conversation: "test1@nightexcessive.us", from: "test1@nightexcessive.us", to: "receiver@nightexcessive.us", time: new Date("2015-02-11T00:12:23.783Z"), message: "Hello 3"}
+		{id: "t1h1", conversation: "test1@nightexcessive.us", from: "test1@nightexcessive.us", to: "receiver@nightexcessive.us", time: moment().subtract(diffBeforeDatetime-actionTime).toDate(), message: "Hello 1"},
+		{id: "t1h2", conversation: "test1@nightexcessive.us", from: "test1@nightexcessive.us", to: "receiver@nightexcessive.us", time: moment().subtract(diffBeforeTime-actionTime).toDate(), message: "Hello 2"},
+		{id: "t1h3", conversation: "test1@nightexcessive.us", from: "test1@nightexcessive.us", to: "receiver@nightexcessive.us", time: moment().subtract(diffBeforeFromNow-actionTime).toDate(), message: "Hello 3"},
+		{id: "t1h4", conversation: "test1@nightexcessive.us", from: "test1@nightexcessive.us", to: "receiver@nightexcessive.us", time: moment().toDate(), message: "Hello 4"},
 	]
 });
 
