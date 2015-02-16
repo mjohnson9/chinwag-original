5803f42e5705ebd0a5b15b9e2fc614f5ef0b3631
Michael Johnson <michael@nightexcessive.us>
2015-02-16 14:36:58 -0500

    Lots of changes

--- build.sh
+++ build.sh
@@ -9,3 +9,5 @@ rm -rf dist
 ember build --environment=production
 rm -rfv /srv/www/xmpp-client/*
 mv -v dist/* /srv/www/xmpp-client/
+
+find /srv/www/xmpp-client/ -type f -exec bash -c 'PLAINFILE={};GZIPPEDFILE={}.gz;gzip -c "$PLAINFILE" > "$GZIPPEDFILE";' \;
