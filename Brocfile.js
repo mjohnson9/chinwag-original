a6df371e404a6080c38c580c0a365c889aeceac5
Michael Johnson <michael@nightexcessive.us>
2015-02-16 13:13:34 -0500

    Add support for application cache

--- Brocfile.js
+++ Brocfile.js
@@ -1,4 +1,6 @@
 /* global require, module */
+var mergeTrees = require('broccoli-merge-trees');
+var manifest = require('broccoli-manifest');
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
@@ -25,4 +27,30 @@ app.import(app.bowerDirectory + '/ember-cli-moment-shim/moment-shim.js', {
 });
 app.import(app.bowerDirectory + '/moment-timezone/moment-timezone.js');
 
-module.exports = app.toTree();
+var tree = app.toTree();
+
+if(app.env == 'production') {
+	var manifestTree = manifest(tree, {
+		appcacheFile: '/manifest.appcache',
+		includePaths: [
+			// Roboto Draft font files
+			//   default font face
+			'https://fonts.gstatic.com/s/robotodraft/v1/0xES5Sl_v6oyT7dAKuoni4gp9Q8gbYrhqGlRav_IXfk.woff2',
+			'https://fonts.gstatic.com/s/robotodraft/v1/0xES5Sl_v6oyT7dAKuoni7rIa-7acMAeDBVuclsi6Gc.woff',
+			//   medium font face
+			'https://fonts.gstatic.com/s/robotodraft/v1/u0_CMoUf3y3-4Ss4ci-VwXJuJo8UJJfpGKt7pXjBv4s.woff2',
+			'https://fonts.gstatic.com/s/robotodraft/v1/u0_CMoUf3y3-4Ss4ci-VwaTA90I55Xt7owhZwpPnMsc.woff',
+			//   bold font face
+			'https://fonts.gstatic.com/s/robotodraft/v1/u0_CMoUf3y3-4Ss4ci-Vwf79_ZuUxCigM2DespTnFaw.woff2',
+			'https://fonts.gstatic.com/s/robotodraft/v1/u0_CMoUf3y3-4Ss4ci-VwRbnBKKEOwRKgsHDreGcocg.woff',
+			//   italic font face
+			'https://fonts.gstatic.com/s/robotodraft/v1/er-TIW55l9KWsTS1x9bTfgeOulFbQKHxPa89BaxZzA0.woff2',
+			'https://fonts.gstatic.com/s/robotodraft/v1/er-TIW55l9KWsTS1x9bTfoo3ZslTYfJv0R05CazkwN8.woff'
+		],
+		fallback: ['* /index.html'],
+		network: []
+	});
+	tree = mergeTrees([tree, manifestTree]);
+}
+
+module.exports = tree;
