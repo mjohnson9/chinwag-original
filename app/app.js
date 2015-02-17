61602a3f1497c8287c04a2842dd6cd72ce564ea5
Michael Johnson <michael@nightexcessive.us>
2015-02-16 22:59:28 -0500

    Move application cache events out of inline script

--- app/app.js
+++ app/app.js
@@ -3,6 +3,22 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
+window.addEventListener('load', function(e) {
+	if(window.applicationCache != null) {
+		window.applicationCache.addEventListener('updateready', function(e) {
+			if(window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+				window.applicationCache.swapCache();
+				window.location.reload();
+			}
+		}, false);
+		window.applicationCache.addEventListener('obsolete', function(e) {
+			if(window.applicationCache.status == window.applicationCache.OBSOLETE) {
+				window.location.reload();
+			}
+		}, false);
+	}
+}, false);
+
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
