e5829fc5e94985e23516ffbf06d2bfd54cbbabf1
Michael Johnson <michael@nightexcessive.us>
2015-02-17 11:40:09 -0500

    Minor build script improvements

--- build.sh
+++ build.sh
@@ -5,9 +5,17 @@ shopt -s dotglob
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+echo "Clearing old dist..."
 rm -rf dist
+
+echo "Building Ember-CLI application..."
 ember build --environment=production
+
+echo "Removing old live code..."
 rm -rfv /srv/www/xmpp-client/*
+
+echo "Moving in new live code..."
 mv -v dist/* /srv/www/xmpp-client/
 
-find /srv/www/xmpp-client/ -type f -exec bash -c 'PLAINFILE={};GZIPPEDFILE={}.gz;gzip -c "$PLAINFILE" > "$GZIPPEDFILE";' \;
+echo "Compressing all live code..."
+find /srv/www/xmpp-client/ -type f -exec bash -c 'PLAINFILE={};GZIPPEDFILE={}.gz;gzip --best -c "$PLAINFILE" > "$GZIPPEDFILE";' \;
