c0ef7bcc320094ad951fadf52c886d42dde2bffb
Michael Johnson <michael@nightexcessive.us>
2015-02-19 16:24:25 -0500

    build.sh: Use zopfli instead of gzip

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ echo "Building Ember-CLI application..."
 ember build --environment=production --output-path="${DEST_DIR}"
 
 echo "Compressing new live code..."
-find "${DEST_DIR}" -type f -not -iname '*.gz' -print0 | xargs -0 --max-procs="$(nproc)" --max-args=1 -- gzip --best --keep --verbose --
+find "${DEST_DIR}" -type f -not -iname '*.gz' -print0 | xargs -0 --max-procs="$(nproc)" --max-args=1 -- zopfli -i1000
