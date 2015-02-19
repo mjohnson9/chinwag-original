7835349d9a2073f92be242ddadb936dae2da4a0c
Michael Johnson <michael@nightexcessive.us>
2015-02-19 16:51:38 -0500

    Remove obsolete refresh code

--- app/app.js
+++ app/app.js
@@ -3,17 +3,6 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
-window.addEventListener('load', function() {
-	if(window.applicationCache != null) {
-		window.applicationCache.addEventListener('obsolete', function() {
-			if(window.applicationCache.status === window.applicationCache.OBSOLETE) {
-				// Due to the special nature of obsolete, we always reload the page.
-				window.location.reload();
-			}
-		}, false);
-	}
-}, false);
-
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
